f66c7b791f345313847ec686bc50052f6e91f12e
ZvegintsevRoman <zvegintsev.roma@yandex.by>
2019-10-10 02:52:36 +0300

    fix: accelerated algorithm

--- src/index.js
+++ src/index.js
@@ -2,23 +2,20 @@ module.exports = function zeros(expression) {
   let oneStepFactorial = [];
   let twoStepFactorial = [];
   let fives = 0;
+  let twos = 0;
 
   expression.split('*').map(str => str.endsWith('!!') ? twoStepFactorial.push(str.slice(0, -2)) : oneStepFactorial.push(str.slice(0, -1)));
   
-  if (twoStepFactorial.every(item => item % 2 != 0) && !oneStepFactorial.length) twoStepFactorial = [];
-
   function countZeros(num, step) {     
-    for (num; num > 0; num -= step) {
-      let current = num;
-      while (current > 0) {
-        if (current % 5 === 0) fives++; 
-      current /= 5;
-      }   
+    for (num; num > 0; num -= step) {      
+      if (num % 25 === 0) fives++;
+      if (num % 5 === 0) fives++;      
+      if (num % 2 === 0) twos++; 
     }     
   }  
 
   oneStepFactorial.forEach(num => countZeros(num, 1));
   twoStepFactorial.forEach(num => countZeros(num, 2));
   
-  return fives;
+  return Math.min(twos,fives);
 }
